7e87ac77773f9c00e912bef2495296d9599effcd
RichardInnocent <richardinnocent@sky.com>
2021-11-20 17:06:53 +0000

    Added story for label component

---
+++ src/components/inputs/label.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, Story } from "@storybook/react";
+import React from "react";
+import Label from "./label";
+
+export default {
+  component: Label,
+  title: "Inputs/Label",
+} as Meta;
+
+interface LabelStoryProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  value: string;
+}
+
+const Template: Story<LabelStoryProps> = (props: LabelStoryProps) => {
+  return <Label>{props.value}</Label>;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  value: "A label",
+};
--- src/components/inputs/label.tsx
+++ src/components/inputs/label.tsx
@@ -1,6 +1,9 @@
+import React from "react";
 import styled from "styled-components";
 import colours from "../../common/colours/colours";
 
-export const Label = styled.label`
+export const Label = styled.label<React.LabelHTMLAttributes<HTMLLabelElement>>`
   color: ${colours.colour1};
 `;
+
+export default Label;
