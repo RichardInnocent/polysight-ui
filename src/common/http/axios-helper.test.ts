656f7ef6dc72fd13f0e1164bd3278a30ae818e88
RichardInnocent <richjinnocent@gmail.com>
2021-12-31 15:44:50 +0000

    Added a helper to provide error wrapping from Axios responses

---
+++ src/common/http/axios-helper.test.ts
@@ -0,0 +1,74 @@
+import {AxiosResponse} from "axios";
+import {throwIfNot2xx} from "./axios-helper";
+
+describe("axios-helper", () => {
+    describe("throwIfNot2xx", () => {
+        const errorMessagePrefix = "Non-2xx status code returned";
+        const simpleResponseBody = "Test response body";
+
+        it("does not throw an error if the response code is 200", () => {
+            throwIfNot2xx(createAxiosResponseMock(200, simpleResponseBody));
+        });
+
+        it("does not throw an error if the response is 299", () => {
+            throwIfNot2xx(createAxiosResponseMock(299, simpleResponseBody));
+        });
+
+        it("throws an error if the response is 199", () => {
+            try {
+                throwIfNot2xx(createAxiosResponseMock(199, simpleResponseBody));
+                fail("No error thrown");
+            } catch (e: unknown) {
+                expectErrorMessage(e).toBe(`${errorMessagePrefix}. Response: ${simpleResponseBody}`);
+            }
+        });
+
+        it("throws an error if the response is 300", () => {
+            try {
+                throwIfNot2xx(createAxiosResponseMock(300, simpleResponseBody));
+                fail("No error thrown");
+            } catch (e: unknown) {
+                expectErrorMessage(e).toBe(`${errorMessagePrefix}. Response: ${simpleResponseBody}`);
+            }
+        });
+
+        describe("the response has a status code of 400", () => {
+            const statusCode = 400;
+
+            it("throws an error but does not append a reason if the response is empty", () => {
+                try {
+                    throwIfNot2xx(createAxiosResponseMock(statusCode, ""));
+                    fail("No error thrown");
+                } catch (e: unknown) {
+                    expectErrorMessage(e).toBe(`${errorMessagePrefix}`);
+                }
+            });
+
+            it("throws an error and converts the response to string if the response contains JSON", () => {
+                try {
+                    throwIfNot2xx(createAxiosResponseMock(statusCode, {
+                        reason: "Invalid request",
+                    }));
+                    fail("No error thrown");
+                } catch (e: unknown) {
+                    expectErrorMessage(e).toBe(`${errorMessagePrefix}. Response: {"reason":"Invalid request"}`);
+                }
+            });
+        });
+    });
+});
+
+function createAxiosResponseMock<Type>(status: number, body: Type): AxiosResponse<Type> {
+    return {
+        data: body,
+        status: status,
+    } as AxiosResponse<Type>
+}
+
+function expectErrorMessage(e: unknown): jest.JestMatchers<string> {
+    if (e instanceof Error) {
+        return expect(e.message);
+    } else {
+        fail(`e is not an Error, but is instead of type ${typeof e}`)
+    }
+}
---
+++ src/common/http/axios-helper.ts
@@ -0,0 +1,17 @@
+import { AxiosResponse } from "axios";
+
+export const throwIfNot2xx = (response: AxiosResponse): void => {
+    if (response.status < 200 || response.status >= 300) {
+        const responseBody = convertBodyToString(response)
+        throw new Error(`Non-2xx status code returned${responseBody.length > 1 ? '. Response: ' + responseBody : ''}`)
+    }
+}
+
+const convertBodyToString = (response: AxiosResponse): string => {
+    const body = response.data;
+    if (typeof body === "string") {
+        return String(response.data);
+    } else {
+        return JSON.stringify(body);
+    }
+}
