f8f74bc8db1f4bed070b8a626ce65f864fd8c4e9
RichardInnocent <richjinnocent@gmail.com>
2022-01-08 19:46:32 +0000

    Fixed rendering of spinner inside Storybook

--- src/common/styling/globalStyle.ts
+++ src/common/styling/globalStyle.ts
@@ -7,7 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     src: url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap");
   }
 
-  * {
+  .sb-show-main {
     font-family: 'Open Sans', sans-serif;
     background: ${colours.colour8};
     color: ${colours.colour1};
